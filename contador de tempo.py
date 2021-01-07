2c25d098dde04848dd17c0c2bf47f0b54e1a61ec
EduardoGrr <77083250+EduardoGrr@users.noreply.github.com>
2021-01-07 16:40:24 -0300

    Add files via upload

---
+++ contador de tempo.py
@@ -0,0 +1,15 @@
+#Contador_de_tempo
+
+tempo = 15
+
+if tempo == 1:
+  print('1 segundo')
+
+if tempo < 60:
+  print(f'{tempo} segundos')
+
+if tempo == 60:
+    print('1 minuto')
+
+if tempo >= 60:
+    print('sei contar mais não')
